2f306ec75b417b94e294c10e7aa909ad2f4854c9
KALVIUM23 <rockstarsagarkv206@gmail.com>
2025-04-23 11:30:20 +0530

    miletone32

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import './App.css'
-import Login from './pages/login'
+import Login from './pages/Login'
 import Home from './pages/Home'
 import Createproudct from './components/Createproudct'
 import SellerProcutPage from "./pages/SellerProductPage"
